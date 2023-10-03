17dfacba7ba4e006963b4af1094432c69366caf2
Adarsh <kpadarsh6801@gmail.com>
2023-10-04 00:30:42 +0530

    home component design completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSelectChange } from '@angular/material/select';
+import { TodoComponent } from '../todo/todo.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  todos: any = [];
+  filteredTodos: any[] = [
+    { title: 'Task 1', description: 'Description 1', status: 'OPEN' },
+    { title: 'Task 2', description: 'Description 2', status: 'IN_PROGRESS' },
+    { title: 'Task 3', description: 'Description 3', status: 'DONE' },
+    // Add more todo items here
+  ];
+
+  constructor(private apiService: ApiService,
+              private dialog: MatDialog) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  // tslint:disable-next-line:typedef
+  filterChanged(ev: MatSelectChange) {
+    const value = ev.value;
+    this.filteredTodos = this.todos;
+    if (value) {
+      this.filteredTodos = this.filteredTodos.filter(t => t.status === value);
+      console.log(this.filteredTodos);
+    } else {
+      this.filteredTodos = this.todos;
+    }
+  }
+
+  // tslint:disable-next-line:typedef
+  openDialog() {
+    const dialogRef = this.dialog.open(TodoComponent, {
+      width: '500px',
+      hasBackdrop: true,
+      role: 'dialog',
+      height: '500px'
+    });
+
+    dialogRef.afterClosed().subscribe(data => {
+
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  statusChanged(ev: MatSelectChange, todoId: number, index: number) {
+    const value = ev.value;
+
+  }
+
+  // tslint:disable-next-line:typedef
+  delete(id: number) {
+    if (confirm('Do you want to remove the Todo?')) {
+
+    }
+  }
+
+  
 
 }
