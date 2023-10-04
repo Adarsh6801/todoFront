cb4b0b5187e395c40c826e33a73c3eb38efc2940
Adarsh <kpadarsh6801@gmail.com>
2023-10-04 11:29:44 +0530

    auth guard setuped

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, map, tap } from 'rxjs';
+import { ApiService } from '../services/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private apiService:ApiService,
+              private router:Router
+    ) {
+    
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.apiService.jwtUserToken.pipe(
+      map((result)=>!!result),
+      tap((result:boolean)=>{
+        if(!result){
+          this.router.navigateByUrl('/login').then();
+          return result;
+        }
+        return result;
+      })
+    )
   }
   
 }
